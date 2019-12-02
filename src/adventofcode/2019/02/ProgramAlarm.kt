6bfbef437a074370277d19473430179e302ba861
sageprice <sageeprice@gmail.com>
2019-12-02 13:32:58 -0800

    Set up for day 2

---
+++ src/adventofcode/2019/02/ProgramAlarm.kt
@@ -0,0 +1,15 @@
+package adventofcode.`2019`.`02`
+
+import java.io.File
+
+/**
+ * Solution for [Advent of Code day 2](https://adventofcode.com/2019/day/2).
+ */
+fun main() {
+    // Read input file
+    val programSpec: List<Int> =
+        File("src/adventofcode/2019/02/input.txt")
+            .readLines()
+
+    // Part 1
+}
