cfad5b3dc176f11b80eebae2264660d411e1b9cf
sageprice <sageeprice@gmail.com>
2019-12-05 05:32:15 -0800

    Day 4 part 2 solution

--- src/adventofcode/2019/04/SecureContainer.kt
+++ src/adventofcode/2019/04/SecureContainer.kt
@@ -11,27 +11,60 @@ fun main() {
   val high = 893698
 
   // Part 1
-  var satisfactoryPasswords = 0
-  for (i in low..high) {
-    var copy = i
-    var previousDigit = 10
-    var isAscendingDigits = true
-    var hasSameAdjacentDigits = false
-    while (copy > 0) {
-      val lastDigit = copy % 10
-      isAscendingDigits = lastDigit <= previousDigit
-      if (!isAscendingDigits) {
-        break
-      }
-      if (lastDigit == previousDigit) {
-        hasSameAdjacentDigits = true
-      }
-      copy /= 10
-      previousDigit = lastDigit
+  val possiblePasswords = 
+      (low..high).filter { hasNonDecreasingDigits(it) && hasRepeatedDigit(it) }
+  println("Part 1: ${possiblePasswords.size}")
+
+  // Part 2
+  val constrainedPws: List<Int> = 
+      (low..high)
+          .filter { hasNonDecreasingDigits(it) && hasDigitRepeatLengthTwo(it) }
+  println("Part 2: ${constrainedPws.size}")
+}
+
+/** 
+ * Returns true IFF the given integer has some adjacent digits which are equal. 
+ */
+fun hasRepeatedDigit(i: Int): Boolean {
+  var copy = i
+  var previousDigit = 10
+  while (copy > 0) {
+    val lastDigit = copy % 10
+    if (lastDigit == previousDigit) {
+      return true
     }
-    if (isAscendingDigits && hasSameAdjacentDigits) {
-      satisfactoryPasswords++
-    }
+    copy /= 10
+    previousDigit = lastDigit
   }
-  println("Part 1: $satisfactoryPasswords")
+  return false
+}
+
+/** 
+ * Returns true IFF the given integer is a sequence of non-decreasing digits.
+ * E.g. inputs of 123456, 11578, 8999999999 all return true. 10 returns false.
+ */
+fun hasNonDecreasingDigits(i: Int): Boolean {
+  var copy = i
+  var previousDigit = 10
+  while (copy > 0) {
+    val lastDigit = copy % 10
+    if (lastDigit > previousDigit) return false
+    copy /= 10
+    previousDigit = lastDigit
+  }
+  return true
+}
+
+fun hasDigitRepeatLengthTwo(i: Int): Boolean {
+  var copy = i
+  while (copy > 0) {
+    var acc = 0
+    val lastDigit = copy % 10
+    while (copy % 10 == lastDigit) {
+      copy /= 10
+      acc = acc * 10 + lastDigit
+    }
+    if (acc > 10 && 100 > acc) return true
+  }
+  return false
 }
