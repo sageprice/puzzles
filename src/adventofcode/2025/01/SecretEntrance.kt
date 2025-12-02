95123687e5ae9320f17e01f3b517b762e33f4307
sageprice <sageeprice@gmail.com>
2025-12-01 21:57:36 -0500

    2025 Day 1
    Nice and easy

---
+++ src/adventofcode/2025/01/SecretEntrance.kt
@@ -0,0 +1,63 @@
+package adventofcode.`2025`.`01`
+
+import java.io.File
+
+fun main() {
+  val rotations = File("src/adventofcode/2025/01/input.txt").readLines()
+    .map { l ->
+      val dir = l.take(1)
+      val d = l.substring(1).toInt()
+      if (dir == "L") {
+        Left(d)
+      } else {
+        Right(d)
+      }
+    }
+  println(countZeroHits(rotations))
+  println(countZeroClicks(rotations))
+}
+
+private fun countZeroHits(rotations: List<Rotation>): Int {
+  var p = 50
+  var zeros = 0
+  rotations.forEach { rot ->
+    p = when (rot) {
+      is Left -> (p + 100 - rot.d) % 100
+      is Right -> (p + rot.d) % 100
+    }
+    if (p == 0) {
+      zeros += 1
+    }
+  }
+  return zeros
+}
+
+private fun countZeroClicks(rotations: List<Rotation>): Int {
+  var p = 50
+  var zeros = 0
+  rotations.forEach { rot ->
+    p = when (rot) {
+      is Left -> {
+        val d = rot.d % 100
+        zeros += (rot.d / 100)
+        if (p != 0 && p <= d) {
+          zeros += 1
+        }
+        (p + 100 - d) % 100
+      }
+      is Right -> {
+        val d = rot.d % 100
+        zeros += (rot.d / 100)
+        if (p != 0 && p + d >= 100) {
+          zeros += 1
+        }
+        (p + d) % 100
+      }
+    }
+  }
+  return zeros
+}
+
+private sealed class Rotation
+private data class Left(val d: Int): Rotation()
+private data class Right(val d: Int): Rotation()
